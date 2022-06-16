c971bd272a99df1e243c98f1e91155b436aa44a9
Wataru Watanabe <wataru.chocola@gmail.com>
2022-06-16 15:28:02 +0900

    Add scratch card's image

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,7 @@ export interface Env {
   // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
   // MY_DURABLE_OBJECT: DurableObjectNamespace;
   //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
+  SCRATCHCARD_BUCKET: R2Bucket;
 }
 
 export default {
@@ -25,6 +24,12 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    return new Response("Hello World!");
+    const image = await env.SCRATCHCARD_BUCKET.get("genbaneko.png");
+    if (!image) {
+      return new Response("Object Not Found", { status: 404 });
+    }
+
+    return new Response(image.body);
+    //return new Response("Hello World!");
   },
 };
